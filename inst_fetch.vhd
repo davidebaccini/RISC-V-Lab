88d9977f520f9e65a0a471075dd2b97d7d2a0a79
Robin Bertolini <robin.bertolini@studenti.unitn.it>
2026-04-14 10:01:19 +0200

    included everything in inst_fetch.vhd

--- inst_fetch.vhd
+++ inst_fetch.vhd
@@ -1,15 +1,53 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+
 entity inst_fetch is
     port(
-        pc_load: in std_logic;
-        pc_in: in std_logic_vector(11 downto 0);
-        next_pc: out std_logic_vector(11 downto 0);
-        curr_pc: out std_logic_vector(11 downto 0);
-        instruction: out std_logic_vector(31 downto 0)
+        clk         : in std_logic;                     
+        pc_load     : in std_logic;                     
+        pc_in       : in std_logic_vector(11 downto 0); 
+        next_pc     : out std_logic_vector(11 downto 0);
+        curr_pc     : out std_logic_vector(11 downto 0);-- PC attuale
+        instruction : out std_logic_vector(31 downto 0) 
     );
 end entity;
+
 architecture Behavioral of inst_fetch is
+
+    component PC is
+        port(
+            pc_in   : in std_logic_vector(11 downto 0);
+            load_en : in std_logic;
+            pc      : out std_logic_vector(11 downto 0);
+            clka    : in std_logic  
+        );
+    end component;
+
+    component addthe4 is
+        port(
+            pc      : in std_logic_vector(11 downto 0);
+            next_pc : out std_logic_vector(11 downto 0)
+        );
+    end component;
+
+    signal internal_pc : std_logic_vector(11 downto 0);
+
 begin
+    PROGRAM_COUNTER: PC 
+        port map(
+            pc_in   => pc_in,
+            load_en => pc_load,
+            pc      => internal_pc,
+            clka    => clk
+        );
+
+    PC_INCREMENTER: addthe4 
+        port map(
+            pc      => internal_pc,
+            next_pc => next_pc
+        );
+
+    curr_pc <= internal_pc;
+
 end architecture Behavioral;
